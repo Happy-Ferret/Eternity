97713527b61da4cdfe3f57cafb589a03ed95f184
Happy-Ferret <MarkBauer@gmx.ch>
2015-04-24 00:39:46 +0200

    Added develop.sh initial setup script.

---
+++ Voice/develop.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#Mac
+
+echo 'export PATH=$PATH:/users/$USER/Downloads/festival/bin/' >> ~/.bash_profile
+echo 'export FESTVOXDIR=/users/$USER/Downloads/festvox' >> ~/.bash_profile
+echo 'export ESTDIR=/users/$USER/Downloads/speech_tool' >> ~/.bash_profile
